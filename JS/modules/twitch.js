3d90d265e9a83a6f6305f791e3beeb845059abd3
Althalas <martin.petitcontactpro@gmail.com>
2025-08-16 18:03:18 +0200

    twitch maj

--- JS/modules/twitch.js
+++ JS/modules/twitch.js
@@ -1,12 +1,18 @@
 export function initializeTwitchEmbeds() {
-  const streamerChannel = "rorolebateau";
-  const parentDomain = "jeurorofrontend.vercel.app";
+  const streamerChannel = "rorolebateau"; // Changez ici pour le nom de la chaîne que vous voulez afficher
+  const parentDomain = window.location.hostname; // On utilise le domaine actuel pour le paramètre parent
 
+  console.log("Domaine détecté pour l'intégration Twitch:", parentDomain);
+
+  // Création du lecteur vidéo
   new Twitch.Embed("twitch-embed", {
     width: "100%",
     height: "100%",
     channel: streamerChannel,
     layout: "video",
-    parent: [parentDomain],
+    parent: [parentDomain], // Requis par Twitch pour la sécurité
   });
 }
+
+// On attend que la page soit entièrement chargée pour initialiser les embeds
+window.addEventListener("load", initializeTwitchEmbeds);
